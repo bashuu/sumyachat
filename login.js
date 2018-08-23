3df27fd4696d7a5902e76c1c0161775680e015aa
bashuu <baskhuu29@yahoo.com>
2018-08-23 13:58:03 +0800

    fixed a bug after logging in not going to chat

--- login.js
+++ login.js
@@ -1,29 +1,24 @@
+auth.onAuthStateChanged(function(user) {
+    if (user) {
+        window.location = "chat.html"
+    } 
+});
+
 function login(){
     var email = document.getElementById('uname').value;
     var curPsw = document.getElementById('psw').value;
     
-    firebase.auth().signInWithEmailAndPassword(email, curPsw).catch(function(error) {
-        // Handle Errors here.
+    auth.signInWithEmailAndPassword(email, curPsw).catch(function(error) {
         var errorCode = error.code;
         var errorMessage = error.message;
         console.log(errorCode);
         document.getElementById("error").innerHTML = errorMessage;
         document.getElementById("error").style.display = "block";
-        // ...
       });
-
 }
 
 function signup(){
     window.location = "signup.html";
 }
-
-auth.onAuthStateChanged(function(user) {
-    if (user) {
-        window.location = "chat.html"
-    } else {
-      // No user is signed in.
-    }
-});
 
 
